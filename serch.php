f1cefe4effb335666806c07fce105ab121973b14
kwakdh <32162738+kwakdh@users.noreply.github.com>
2018-08-30 10:42:44 +0900

    Update serch.php

--- serch.php
+++ serch.php
@@ -4,26 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
     <title>다희의 게시판</title>
 
     <!-- 부트스트랩 -->
     <link href="http://localhost/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE8 에서 HTML5 요소와 미디어 쿼리를 위한 HTML5 shim 와 Respond.js -->
-    <!-- WARNING: Respond.js 는 당신이 file:// 을 통해 페이지를 볼 때는 동작하지 않습니다. -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <br/>
     <center><h1>게 시 글 찾 기</h1></center>
     <br/>
 </head>
 <body>
-<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
@@ -40,11 +30,9 @@ include('dbconnect.php');
 
 $mysql_handler = connectDB();
 mysqli_select_db($mysql_handler, "new");
-//selectDB($mysql_handler);
 
-//$board_id=$_GET['board_id'];
 
-//검색 옵션 관련
+//About search options
 $serching = $_REQUEST['serching'];//키워드
 $filed = $_REQUEST['filed'];
 
@@ -73,20 +61,16 @@ $total_su = mysqli_num_rows($result);
 echo "<div style='text-align:left '>" . "총 검색된 게시글 수: " . $total_su . "</div>";
 echo "<br/>";
 $page = $_GET[page];
-//--------------------------- 페이지네이션 부분---------------------------------
+//--------------------------- Pagenation section---------------------------------
 
 if (empty($page)) {
     $page = 1;
 }
-$start_no = ($page - 1) * 5; //현재쪽의 첫번째 번호
-
-/* if($start_no>1){
-  mysqli_data_seek($result, $start_no); // result set 에서 원하는 순번의 데이터를 선택.
- }*/
+$start_no = ($page - 1) * 5; //the first number of the present
 
 $total_page = ceil($total_su / 5); //맨 밑에 뜨는 번호 개수
 
-// 만약 검색 결과가 없을 경우
+// If there are no search results
 if (mysqli_num_rows($result) == 0)
     echo "<tr><td colspan=5 align=center height=50>
         	            등록된 글이 없습니다.
@@ -95,7 +79,7 @@ if (mysqli_num_rows($result) == 0)
 
 $line = 0;
 
-//--------------------------- 게시판 body부분 ----------------------------------
+//--------------------------- Border body part ----------------------------------
 
 
 echo "<center>";
@@ -132,14 +116,13 @@ echo "</center>";
 
 ?>
 
-<!------------------------- 쪽 이동 ------------------------------------------>
+<!--------------------------------- 쪽 이동 ------------------------------------->
 
 <table cellspacing=0 border=0 width=80%>
     <tr>
         <td align="center" bgcolor="#F0F0F0">
             <?
             //밑에 수 표시
-
             for ($i = 1; $i <= $total_page; $i++) {
 
                 if ($i == $page)
@@ -155,7 +138,7 @@ echo "</center>";
 
 <?
 
-// -------------------------------되돌아 가자---------------------------------
+// -------------------------------Return part---------------------------------
 
 echo "<center>";
 echo "<input type='button' class='form-control' style='width: 10%'  value='back' onclick=location.href='list.php?page=1'>";
