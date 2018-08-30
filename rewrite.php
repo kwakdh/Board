e204e5a2dc4b6201ae94a366eb2e5c22ed1d4e9b
kwakdh <32162738+kwakdh@users.noreply.github.com>
2018-08-30 10:39:12 +0900

    Update rewrite.php

--- rewrite.php
+++ rewrite.php
@@ -10,20 +10,11 @@
 
     <!-- 부트스트랩 -->
     <link href="http://localhost/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE8 에서 HTML5 요소와 미디어 쿼리를 위한 HTML5 shim 와 Respond.js -->
-    <!-- WARNING: Respond.js 는 당신이 file:// 을 통해 페이지를 볼 때는 동작하지 않습니다. -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <br />
     <center><h1>게 시 글 수 정</h1></center><br/>
 </head>
 <body>
-<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
@@ -45,7 +36,6 @@ include('dbconnect.php');
 
 $mysql_handler = connectDB();
 mysqli_select_db($mysql_handler,"new");
-//selectDB($mysql_handler);
 
 $sql ="select * from board where board_id='$board_id'";
 $result = mysqli_query($mysql_handler,$sql);
@@ -55,6 +45,8 @@ $contents2= $row['contents'];
 
 echo "<center>";
 echo "<div class='container'>
+
+	//<--------------------------------- Poster Modification Form ------------------------------------------->
 	
 	<form class='form-horizontal' action='rewrite2.php?board_id=$board_id' method='post'>
 	  <div class='form-group''>
